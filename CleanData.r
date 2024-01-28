1932a4c3d780e77eeeb2d5870e0435a33d656c28
Ilyas Khallouki <khalloukielias@gmail.com>
2024-01-28 13:33:57 +0100

    Finished the data cleaning script

--- CleanData.r
+++ CleanData.r
@@ -1,3 +1,15 @@
 library("readr")
 
 data <- read_csv("TrainingUnclean.csv")
+
+clean_text <- function(text) {
+  # Remove punctuation
+  text <- tolower(text)
+  text <- gsub("[^a-z0-9 ]", "", text)
+  return(text)
+}
+
+data$Context <- sapply(data$Context, clean_text)
+data$Response <- sapply(data$Response, clean_text)
+
+write_csv(data, "TrainingClean.csv")
